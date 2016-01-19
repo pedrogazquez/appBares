9462548d84b782a38bd1e68933d250cb1748a9cb
pedrogazquez <pedrogazqueznavarrete@gmail.com>
2016-01-19 10:30:36 +0100

    debug false

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -108,5 +108,7 @@ STATICFILES_DIRS = (
     STATIC_PATH,
 )
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Absolute path to the media directory
